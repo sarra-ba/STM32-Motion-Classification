63c45e5bf3a5cbd8d316ceb4fcb9b8f12e754c05
sarra-ba <sarra.baati@supcom.tn>
2025-12-10 09:22:51 +0100

    Add files via upload

---
+++ STM32_Project/X-CUBE-AI/App/network_data_params.c
@@ -0,0 +1,73 @@
+/**
+  ******************************************************************************
+  * @file    network_data_params.c
+  * @author  AST Embedded Analytics Research Platform
+  * @date    2025-12-08T23:21:24+0100
+  * @brief   AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  ******************************************************************************
+  */
+
+#include "network_data_params.h"
+
+
+/**  Activations Section  ****************************************************/
+ai_handle g_network_activations_table[1 + 2] = {
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+  AI_HANDLE_PTR(NULL),
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+};
+
+
+
+
+/**  Weights Section  ********************************************************/
+AI_ALIGNED(32)
+const ai_u64 s_network_weights_array_u64[126] = {
+  0xb94de7eaa5557e2U, 0xd4476da7b025b557U, 0xa517fc51c3f467fU, 0xcf03d620448c26f3U,
+  0xad742012fcdb1296U, 0x558171300350626eU, 0x2fffe239e80e9c2fU, 0xefb65f927f1fdc2aU,
+  0xc6e435ee24ef21f0U, 0x3d35e7c3dfa474f8U, 0x3031cf2498177fbdU, 0x819ade0aae13d507U,
+  0xf964a022602e345fU, 0x81c3dafe23122f38U, 0x265232daef5b417eU, 0x1c4c37aecf60bca3U,
+  0x44f57f53df9ff6b6U, 0x15d4c955113e32caU, 0x1140000022cU, 0x1b600000267U,
+  0x2ec00000241U, 0x5170000046dU, 0x9c8e2935d6a85a40U, 0xdff022dc1ee12b01U,
+  0xdf023aea01147fb8U, 0x3024616081870354U, 0xb48eb5342a057aedU, 0x94777af64f99bd2dU,
+  0x4734014602cc5c6aU, 0x24d1bc3654cd112bU, 0xc09ae913c1a67f39U, 0xd3d20e36efd76810U,
+  0xeefaf624161057c3U, 0xf51bb23dd30d4681U, 0x19f30d562cb8a5dfU, 0xfb34a7004fc33fc1U,
+  0x605725561690c381U, 0x11eb297f61d909dbU, 0xe148420d089f41d2U, 0x2849105e30fde335U,
+  0xbd5e3c97096f3238U, 0x1fc829b5f50565fdU, 0xca2edd2bfe7ff660U, 0x4920ffd3e56e3712U,
+  0xb03d4e6eb5f17e6U, 0x2b42e4c1c07fdc3eU, 0x69b46eecfebfa198U, 0xf7617f47d9fce12aU,
+  0x274cb7a8712a05f6U, 0x1c05e8262742151cU, 0x341c4c02f080fedU, 0xd52dfa9aec7f04dbU,
+  0xc94455d5a3fe8aa5U, 0x12fb6a4c81e1f50eU, 0x23cd642cfe36e101U, 0x574c4a413e8d92d1U,
+  0xdf240d157fb8becfU, 0x21757674de9a91c0U, 0xda2ef4c04801f61dU, 0x21ed3f1bc7ed81acU,
+  0x4533452398110ddeU, 0xa84d4b03633ddb09U, 0x39cf254dcd368194U, 0x52c76609f521baf6U,
+  0x1a2aa4ae76c87976U, 0x2a332b173ed9c648U, 0x51d00d25caf27f1bU, 0xe848f4f1b456c121U,
+  0xf8223e06f378c6caU, 0x54e93be2e07f42fdU, 0xfffffe88000004abU, 0x3370000057fU,
+  0x82000000e5U, 0x3fa00000229U, 0x21700000043U, 0xe70000065cU,
+  0x471000001afU, 0x32b00000197U, 0x2288a4c699de817aU, 0x1af7b1d63e2238edU,
+  0x4225138135520142U, 0x43f9bfc2accd3befU, 0x7f72fa82fa8ef4d3U, 0x23f83fbe867d5e1bU,
+  0xe53348755e04063cU, 0xff3d819af9e8c3a0U, 0x83269c65de2dfbb2U, 0x839d7f18cb751bb3U,
+  0xcedb4e4858780d49U, 0xca18d3a3e581d739U, 0xec45296881a6ee88U, 0xea30595c5907207eU,
+  0xbccd591f3d7f1afcU, 0xd336c6c5629d890dU, 0xfd282de481ce5d0fU, 0x84edfe10e394208cU,
+  0xec212856eab304daU, 0x96ef3bd77ff1f738U, 0x81b2627888183aaeU, 0xaa1c04d44860335dU,
+  0xe9db45246cb9c381U, 0xafd3e540ab0c9a2bU, 0x1693d448faf68864U, 0xe85be9b430341f81U,
+  0xd7f639dcafbf2088U, 0x19366e5c7f71ebfeU, 0xd10436119bfd132eU, 0x10cbcb207f09c6f5U,
+  0xe74f1bd9737d215cU, 0x9e3ba0cd81dbc2cfU, 0x86000000000U, 0xfffffdc40000037bU,
+  0x45600000000U, 0xfffffea70000032dU, 0xfffffb4effffff4aU, 0xfffffd56U,
+  0xffffff3efffffb70U, 0x45500000026U, 0x3ef401f7f9a79004U, 0xc97534df026c7f10U,
+  0x3881583629ff471aU, 0x3ddea2cef0c8f8e4U, 0x81f784dfd44b4101U, 0xc19c3a3030f6dac3U,
+  0x10bfffff432U, 0x9e5U,
+};
+
+
+ai_handle g_network_weights_table[1 + 2] = {
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+  AI_HANDLE_PTR(s_network_weights_array_u64),
+  AI_HANDLE_PTR(AI_MAGIC_MARKER),
+};
+
